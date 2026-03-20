61683084fd6a5fef291430febef4e1565962da8a
mamorin <mamosanhaeiyuudesu@yahoo.co.jp>
2026-03-20 22:25:05 +0900

    feat: favicon.svgを追加し、アプリのアイコンを設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,13 @@ import { defineNuxtConfig } from 'nuxt/config';
 export default defineNuxtConfig({
   srcDir: 'src',
   compatibilityDate: '2026-03-12',
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+      ],
+    },
+  },
   modules: ['@nuxtjs/tailwindcss'],
   runtimeConfig: {
     openaiApiKey: '',
